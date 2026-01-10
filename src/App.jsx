aa36759f81e39390d173dd9b77e4c20bcd3910d5
fachryikhsal <fachryikhsal890@gmail.com>
2026-01-10 19:49:50 +0700

    feat: add sidebar and active item

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,48 @@
+import SidebarItem from "./components/SidebarItem";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // Active SideItem - Course List
+  const [activeSidebar, setActiveSidebar] = useState("course_list");
+
+  // Sidebar Click Handler
+  const handleSidebarItemClick = (item) => {
+    setActiveSidebar(item);
+    console.log(item);
+  };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center p-8 text-white">
-      <div className="bg-white/10 backdrop-blur-xl rounded-3xl p-12 shadow-2xl max-w-md w-full text-center border border-white/20">
-        <div className="flex justify-center space-x-8 mb-8">
-          <a href="https://vite.dev" target="_blank">
-            <img
-              src={viteLogo}
-              className="w-20 h-20 hover:scale-110 transition"
-              alt="Vite"
-            />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img
-              src={reactLogo}
-              className="w-20 h-20 hover:rotate-12 transition"
-              alt="React"
-            />
-          </a>
+    <div className="min-h-screen bg-linear-to-br bg-white p-8 text-white flex">
+      <div className="bg-black w-50 flex-col p-5 mr-5">
+        <SidebarItem
+          label="course_list"
+          item_name="Database"
+          isActive={activeSidebar === "course_list"}
+          onClick={() => handleSidebarItemClick("course_list")}
+        />
+        <SidebarItem
+          label="generate_course"
+          item_name="Generate Course"
+          isActive={activeSidebar === "generate_course"}
+          onClick={() => handleSidebarItemClick("generate_course")}
+        />
+      </div>
+
+      <div className="bg-white border border-black flex-1 p-10">
+        {/* Course Item */}
+        <div className="border border-black w-50 p-2">
+          <h1 id="title" className="text-black font-semibold">
+            Artificial Intelligence
+          </h1>
+          <p id="description" className="text-black">
+            This course covers about The fundamental of using AI and it's basic
+            applications.
+          </p>
+
+          <div className="text-black">Lesson Count: 5</div>
+          <div className="text-black">Index Count: 5</div>
+          <div className="text-black">Version: 1</div>
         </div>
-
-        <h1 className="text-4xl font-bold bg-gradient-to-r from-white to-gray-200 bg-clip-text text-transparent mb-8">
-          React + Vite + TailwindCSS
-        </h1>
-
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="w-full bg-gradient-to-r from-emerald-400 to-blue-500 hover:from-emerald-500 hover:to-blue-600 text-white font-bold py-4 px-8 rounded-2xl shadow-xl hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 mb-6 text-xl"
-        >
-          count is {count}
-        </button>
-
-        <p className="text-gray-200">
-          Tailwind working! Responsive + effects active.
-        </p>
       </div>
     </div>
   );
---
+++ src/components/CourseItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CourseItem = () => {
+  return <div>Course Item</div>;
+};
+
+export default CourseItem;
