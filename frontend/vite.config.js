915cd773d60ecbb22d397aa603a4ba2b5fcf71aa
fachryikhsal <fachryikhsal890@gmail.com>
2026-03-01 14:58:16 +0700

    deployment: dockerize project with production and development with hot reload module

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      clientPort: 5173,
+    },
+  },
 });
